c2aff7e9cc4f5623c190df23dff80fccd8501bad
rachaels2515 <44954021+rachaels2515@users.noreply.github.com>
2018-11-13 21:04:11 -0500

    Update citations.py

--- citations.py
+++ citations.py
@@ -3,3 +3,6 @@
 #horse sprite with color options: https://opengameart.org/content/lpc-horses
 #extended horse sprite: https://opengameart.org/content/lpc-horse-extended
 #spritesheet animation: https://www.youtube.com/watch?v=mfX3XQv9lnI
+# 13NOV18
+#field: https://www.istockphoto.com/photos/open-field-blue-sky?sort=mostpopular&mediatype=photography&phrase=open%20field%20blue%20sky
+#grass: https://www.photohdx.com/pic-2105/green-grass-field-texture
