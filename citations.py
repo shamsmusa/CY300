aeb12a77115ab5c8c63134ade7420e945217270c
rachaels2515 <44954021+rachaels2515@users.noreply.github.com>
2018-11-15 01:53:57 -0500

    Update citations.py

--- citations.py
+++ citations.py
@@ -6,3 +6,6 @@
 # 13NOV18
 #field: https://www.istockphoto.com/photos/open-field-blue-sky?sort=mostpopular&mediatype=photography&phrase=open%20field%20blue%20sky
 #grass: https://www.photohdx.com/pic-2105/green-grass-field-texture
+# 14NOV18
+#pic resize: http://www.picresize.com/
+#horse: https://stickeroid.com/?q=clipart%20brown%20horse%20png%20image%20picture,%20transparent%20background%2048
